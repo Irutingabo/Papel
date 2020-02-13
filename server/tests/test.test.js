34d5bf05a9e1b0dcc67f63b345e72b4c03456693
Irutingabo <irutingabos3@gmail.com>
2020-02-13 14:37:09 +0200

    [Finishes #171254240, #171254279] integrating Travis, Coveralls

--- server/tests/test.test.js
+++ server/tests/test.test.js
@@ -1,3 +1,7 @@
+
+// eslint-disable-next-line no-unused-vars
+import { should } from 'chai';
+
 describe('Testing', () => {
     describe('Full setup is done', () => {
         it('Full setup should appear pending');
