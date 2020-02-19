75205bf88cfaf84b0485f6ceb272a878db294f67
Deve Loper <devel605@gmail.com>
2020-02-19 17:32:33 +0200

    Connected dotenv variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,12 @@
 import { Pool } from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const pool = new Pool({
-  user: 'shamiru',
-  host: 'localhost',
-  database: 'papeldb',
-  password: '0002',
+  user: process.env.DB_USER,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+  password: process.env.PASSWORD,
   port: 5432,
 })
 
